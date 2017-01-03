443eb1163788f99c44201f7db4db31f679762446
vlad2 <vlad.sereda.y@gmail.com>
2017-01-03 15:32:48 +0200

    Task 3 commented

--- src/com/userok/learning/softgrouptest/N3StringParser.java
+++ src/com/userok/learning/softgrouptest/N3StringParser.java
@@ -3,29 +3,44 @@ package com.userok.learning.softgrouptest;
 import java.util.ArrayList;
 import java.util.List;
 
+/*Class for displaying numbers from a
+* given string*/
 public class N3StringParser {
 
+    //here the numbers of a string are saved
+    //it contains Numbers, so we can put
+    // not only integers, but also floating point numbers
     private List<Number> numbers;
 
     public N3StringParser(String str) {
+        //first, we divide a string into separate blocks:
+        //single numbers (like "451") or expressions (like "5-4", "*1", "9+")
         String[] strings = str.split("[^0-9^\\*^\\+^\\-^/]+");
 
         List<String> temp = new ArrayList<>(strings.length);
+        //add to list only strings which are single numbers or
+        //correct expressions (number-sign-number)
         for (String string : strings) {
             if (!string.matches("([0-9]+|[0-9]+[\\*\\+\\-/][0-9]+)")) continue;
             temp.add(string);
         }
+        //retrieving a list of numbers
         numbers = strToNums(temp);
     }
 
+    //a function which converts strings to numbers
     private List<Number> strToNums(List<String> temp) {
         List<Number> nums = new ArrayList<>(temp.size());
         for (String string : temp) {
             Number n = null;
             try {
+                //if it is a single number, parse it
                 n = Integer.parseInt(string);
+                //if it's not single number, we get NF exception
             } catch (NumberFormatException ex) {
-                String delimeter = null;
+                //in each "if" statement we check if the sign between
+                //the numbers is a specific to that block sign
+                //and accordingly to this parse the expression
                 if (string.contains("/"))
                 {
                     String[] pair = string.split("/");
@@ -56,6 +71,7 @@ public class N3StringParser {
         return nums;
     }
 
+    //function for displaying a result - to satisfy task conditions
     public void displayResult() {
         System.out.println("Numbers");
         for (Number number : numbers) System.out.println(number.toString());
--- src/com/userok/learning/softgrouptest/N3StringParserTest.java
+++ src/com/userok/learning/softgrouptest/N3StringParserTest.java
@@ -2,7 +2,8 @@ package com.userok.learning.softgrouptest;
 
 public class N3StringParserTest {
     public static void main(String[] args) {
-        N3StringParser parser = new N3StringParser("123_2*14ghfg003sa* *d15/2dasdasd8.10");
+        //N3StringParser parser = new N3StringParser("123_2*14ghfg003sa* *d15/2dasdasd8.10");
+        N3StringParser parser = new N3StringParser("123_2*14ghfg003s*99h99*a* *d15/2dasdasd8.10");
         parser.displayResult();
     }
 }
